f993384dd30a7df465cb6d87a1c078d7c134c011
MichaelShepard <mike@rockcastlepartners.com>
2021-04-08 21:25:16 -0400

    Setup data tables

---
+++ SimpleThx.Data/Friend.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleThx.Data
+{
+    public class Friend
+    {
+
+        public int FriendID { get; set; }
+
+        public int AccountID { get; set; }
+
+        public int FreindsUserID { get; set; }
+
+        public Enum Status { get; set; }
+
+        public DateTimeOffset CreateUTC { get; set; }
+
+        public DateTimeOffset? ModifiedUTC { get; set; }
+
+
+
+
+    }
+}
---
+++ SimpleThx.Data/Post.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleThx.Data
+{
+    public class Post
+    {
+
+        [Key]
+        public int PostID { get; set; }
+
+        public int PostUserID { get; set; }
+
+        public int AboutUserID { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+
+        [Required]
+        public Enum Status { get; set; }
+
+        public DateTimeOffset CreateUTC { get; set; }
+
+        public DateTimeOffset? ModifiedUTC { get; set; }
+
+
+    }
+}
