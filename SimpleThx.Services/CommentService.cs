7a89d39b542d5250b02ee4176379fbf4ee6975b9
MichaelShepard <mike@rockcastlepartners.com>
2021-04-29 20:06:05 -0400

    started comment

---
+++ SimpleThx.Services/CommentService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleThx.Services
+{
+    public class CommentService
+    {
+
+        private readonly Guid _userID;
+
+        public CommentService(Guid userID)
+        {
+            _userID = userID;
+        }
+
+
+
+
+
+
+
+
+
+
+
+    } // END Comment Service Class
+} // END Namespace
---
+++ SimpleThx.webmvc/Controllers/CommentController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SimpleThx.webmvc.Controllers
+{
+
+    [Authorize]
+    public class CommentController : Controller
+    {
+        // GET: Comment
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+
+
+
+    } // END Comment Controller
+} // END Namespace
